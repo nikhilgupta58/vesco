1c1537d28199b557d89d8378df5e7b0536c152a6
Nikhil Gupta <nikhilkumargupta58@gmail.com>
2018-03-19 21:29:51 +0530

    upadted navi

--- js/java.js
+++ js/java.js
@@ -75,3 +75,33 @@ $(function () {
 
     });
 });
+/*================================
+        nav menu
+==================================*/
+$(function () {
+    $(window).scroll(function () {
+
+        if ($(this).scrollTop() < 50) {
+            $("nav").removeClass("vesco-top-nav")
+            $("#back-to-top").fadeOut();
+
+        } else {
+            $("nav").addClass("vesco-top-nav")
+            $("#back-to-top").fadeIn();
+        }
+    });
+});
+/*smooth scrolling*/
+$(function () {
+
+    $("a.smooth-scroll").click(function (event) {
+        event.preventDefault();
+        //get\return id like #about,and etc
+        var section = $(this).attr("href");
+
+        $('html, body').animate({
+            scrollTop: $(section).offset().top - 64
+        }, 1250, "easeInOutExpo");
+    })
+
+});
